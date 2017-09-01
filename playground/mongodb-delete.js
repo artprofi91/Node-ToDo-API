ea15370dce8e9523c2794a0945a79c38d8a611b9
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-09-01 18:03:07 -0500

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err,db)=>{
+    if(err) {
+        return console.log('Unable to connect to MongoDB server.');
+    }
+    console.log('Connected to MongoDB server.');
+
+    //deleteMany
+    // db.collection('Todos').deleteMany({text:'Eat lunch'}).then((result)=>{
+    //     console.log(result);
+    // })
+    // db.collection('Users').deleteMany({name:'Art'}).then((result)=>{
+    //     console.log(result);
+    // })
+    //deleteOne
+    // db.collection('Todos').deleteOne({text:'Eat lunch'}).then((result)=>{
+    //     console.log(result);
+    // });
+    //findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed:false}).then((result)=>{
+    //     console.log(result);
+    // });
+    // db.collection('Users').findOneAndDelete({ _id: new ObjectID("59a8c39bce9dd0544c284029")}).then((result)=>{
+    //     console.log(JSON.stringify(results, undefined, 2));
+    // });
+    // db.close();
+})
